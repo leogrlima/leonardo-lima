1ead8996c96adc9169cf2fdd66691d9bcfc530eb
Leonardo <135643102+leogrlima@users.noreply.github.com>
2024-10-01 16:53:07 -0300

    Add files via upload

--- hemocentro.js
+++ hemocentro.js
@@ -165,7 +165,7 @@ function buscarDoadorUltimaDoacao() {
 ║20│21│22│23│23│24│25║
 ╟──┼──┼──┼──┼──┼──┼──╢
 ║26│27│28│29│30│31│░░║
-╟════════════════════╢
+╚════════════════════╝
 `))
 
     let ultimaDoacao = ask.question(cor("roxo", "Digite a data desejada (dd/mm/aaaa): "))
